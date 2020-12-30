ac4e267b028b205751049fe64ae3670ae627c9ff
omkar <worldomkar@gmail.com>
2020-12-31 01:05:19 +1100

    Update tests

--- InfoTrack.TechChallenge/ClientApp/src/App.test.tsx
+++ InfoTrack.TechChallenge/ClientApp/src/App.test.tsx
@@ -1,22 +1,38 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+// import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-    const storeFake = (state: any) => ({
-        default: () => {},
-        subscribe: () => {},
-        dispatch: () => {},
-        getState: () => ({ ...state })
-    });
-    const store = storeFake({}) as any;
-
-    ReactDOM.render(
-        <Provider store={store}>
-            <MemoryRouter>
-                <App/>
-            </MemoryRouter>
-        </Provider>, document.createElement('div'));
+it('InfoTrack Seo Check App renders without crashing', () => {
+  const storeFake = (state: any) => ({
+    default: () => {},
+    subscribe: () => {},
+    dispatch: () => {},
+    getState: () => ({ ...state }),
+  });
+  const store = storeFake({
+    configuration: {
+      searchEngines: [],
+    },
+  }) as any;
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      </Provider>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+  //   ReactDOM.render(
+  //     <Provider store={store}>
+  //       <MemoryRouter>
+  //         <App />
+  //       </MemoryRouter>
+  //     </Provider>,
+  //     document.createElement('div')
+  //   );
 });
